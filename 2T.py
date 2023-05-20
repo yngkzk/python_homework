b0878d08540d1a7364644f45c1ed98da645bec95
yngkzk <ansar.iskakow@gmail.com>
2023-05-20 16:39:27 +0600

    Create 2T.py
    2 task

---
+++ 2T.py
@@ -0,0 +1,7 @@
+# 2. Запросите у пользователя 2 числа и выведите среднее арифметическое этих чисел.
+users_numb = input("Enter the first number: ")
+users_numb2 = input("Enter the second number: ")
+number = int(users_numb)
+number2 = int(users_numb2)
+result = (number + number2) / 2
+print("Your average number is: ", result)
