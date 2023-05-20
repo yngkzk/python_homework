5451e61eaf99942cca3caa17c91044aa80c21eb1
yngkzk <ansar.iskakow@gmail.com>
2023-05-20 16:39:05 +0600

    Create 8T.py
    8 task

---
+++ 8T.py
@@ -0,0 +1,6 @@
+# 8. Запросите у пользователя трехзначное число и выведите вторую цифру этого числа. Для решения задачи используйте оператор % (остаток от деления).
+tdn = input("Enter three digit number: ")
+n = int(tdn)
+result = n % 100
+result2 = result // 10
+print("The middle of the number equals: ", result2)
