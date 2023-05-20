ff40cda10f484fe60525c34edc2ccbf2e2072090
yngkzk <ansar.iskakow@gmail.com>
2023-05-20 16:39:31 +0600

    Create 1T.py
    1 task

---
+++ 1T.py
@@ -0,0 +1,5 @@
+# 1. Запросите у пользователя число, возведите это число во 2-ю степень и выведите на экран.
+number_req = input("Enter the number: ")
+number = int(number_req)
+result = number ** 2
+print("Your number squared is: ", result)
