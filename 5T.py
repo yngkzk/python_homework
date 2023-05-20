961107e2d389163869491876685d8115414cae7a
yngkzk <ansar.iskakow@gmail.com>
2023-05-20 16:39:17 +0600

    Create 5T.py
    5 task

---
+++ 5T.py
@@ -0,0 +1,10 @@
+# 5. Реализуйте калькулятор. Пользователь вводит два числа, а программа выводит результаты действий + - * / между этими числами.
+users_numb = input("Enter the first number: ")
+users_numb2 = input("Enter the second number: ")
+n1 = int(users_numb)
+n2 = int(users_numb2)
+res1 = n1 + n2
+res2 = n1 - n2
+res3 = n1 * n2
+res4 = n1 / n2
+print("The result of your action: ", "Addition: ", res1, "Subtraction: ", res2, "Multiplication: ", res3, "Division:", res4)
