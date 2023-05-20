bc06485d21fa39f6f42fc40f06db8878557eea4f
yngkzk <ansar.iskakow@gmail.com>
2023-05-20 16:39:24 +0600

    Create 3T.py
    3 task

---
+++ 3T.py
@@ -0,0 +1,5 @@
+# 3. Запросите у пользователя длину стороны квадрата и выведите площадь такого квадрата.
+users_req = input("Enter the length of the side of the square: ")
+users_numb = int(users_req)
+result = users_numb ** 2
+print("Your square area: ", result)
