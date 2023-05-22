c5a5d570b6e4d465287c918f418c0007f6f55f4d
yngkzk <ansar.iskakow@gmail.com>
2023-05-22 12:44:53 +0600

    Update 9T.py
    Поменял решение

--- 9T.py
+++ 9T.py
@@ -1,9 +1,9 @@
 # 9. Запросите у пользователя пятизначное число и переместите последнюю цифру в начало (из числа 12345 должно получиться число 51234). Задачу решить математически!
 fdn = input("Enter five digit number: ")
 x = int(fdn)
-n = x % 10
-f = str(n)
-r = x // 10
-t = str(r)
+lastn = x % 10
+fourn = x // 10
+f = lastn * 10000
+t = fourn
 result = f + t
 print("Your result is: ", result)
