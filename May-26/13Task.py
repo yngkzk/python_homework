23bf707b9f4c556cffaed7b244491e18b90ac912
yngkzk <ansar.iskakow@gmail.com>
2023-05-29 05:54:51 +0600

    Update 13Task.py

--- May-26/13Task.py
+++ May-26/13Task.py
@@ -13,12 +13,3 @@ else:
     form = ("Your length of the side is %.2f")
     message = form % formula
 print(message)
-
-'''VN: Очень хорошая попытка, но пока что в формуле есть ошибка:
-Enter the diameter of the circumscribed circle: 10
-Enter the quantity of the sides: 3
-Your length of the side is 10.47
-
-Длина стороны вписанного треугольника получилась больше, чем диаметр окружности.
-С нетерпением жду ваше исправленное решение :)
-'''
