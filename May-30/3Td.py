d5ebdf68527e43f8284de8fa90c895449bd54e51
yngkzk <ansar.iskakow@gmail.com>
2023-05-31 14:45:57 +0600

    Update 3Td.py

--- May-30/3Td.py
+++ May-30/3Td.py
@@ -1,20 +1,16 @@
 print("This program shows the number you entered is a leap year or not.")
 user_in = input("Enter there the number: ")
 try:
-    year = str(user_in)
-    div1 = year / 4
-    div01 = year // 4
-    div2 = year / 100 
-    div02 = year // 100
+    year = int(user_in)
 except ValueError:
     msg = "Incorrect data, you have to enter a number!"
 except NameError as Nerr:
     print("Incorrect data: " + Nerr + ". You have to enter a number!")
 except TypeError:
     print("Incorrect data! You have to enter a number!")
-if div2 == div02:
+if year / 100 == year // 100:
     msg = "It is not a leap year."
-elif div1 == div01:
+elif year / 4 == year // 4:
     msg = "It is a leap year."
 else:
     msg = "It is not a leap year."
