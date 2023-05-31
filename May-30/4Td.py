dfe6027418b7203df923db65d1acda9cceed6483
yngkzk <ansar.iskakow@gmail.com>
2023-05-31 20:49:21 +0600

    Update 4Td.py

--- May-30/4Td.py
+++ May-30/4Td.py
@@ -6,6 +6,7 @@ try:
     second = users_word[1]
     fourth = users_word[3]
     fifth = users_word[4]
+    length = len(users_word)
 except ValueError:
     msg = "Incorrect data, you have to enter a word!"
 except IndexError:
@@ -14,6 +15,10 @@ except NameError as Nerr:
     print("Incorrect data: " + Nerr + ". You have to enter a word!")
 if first == fifth and second == fourth:
     msg = "Your word is palindrome"
+elif length > 5:
+    print("Your word has more, than 5 letters.")
+elif length < 5:
+    print("Your word has less, than 5 letters.")
 else:
     first != fifth or second != fourth
     msg = "Your word is not palindrome"
