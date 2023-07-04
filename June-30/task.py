526a72b750d00074dbd69757584ac2c3bb99e2f0
yngkzk <ansar.iskakow@gmail.com>
2023-07-04 18:02:20 +0600

    Update task.py

--- June-30/task.py
+++ June-30/task.py
@@ -1,3 +1,3 @@
 # Write a program that returns a number to the user, but with "=" signs on the sides, 4 times.
 # also this program handles an exception if the user writes a fractional number.
-# Don't use .center() method !!
+# Don't use .center() function !!
