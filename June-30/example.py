7da3ac2816d99fcfa123692466157fdc20d2d193
yngkzk <ansar.iskakow@gmail.com>
2023-07-04 18:01:11 +0600

    cw

---
+++ June-30/example.py
@@ -0,0 +1,6 @@
+sentence = 'She sells seashells by the seashore, The shells she sells are surely seashells'
+value = sentence.find('seashells')
+print(value)
+
+value2 = sentence.find('seashells', 0, 9)
+print(value2)
