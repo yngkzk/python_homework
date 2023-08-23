a01ade3f146b094befb97a75dcb6e35ecec5bdb3
yngkzk <ansar.iskakow@gmail.com>
2023-08-23 18:58:44 +0600

    Update 4T.py

--- July-21/4T.py
+++ July-21/4T.py
@@ -1,33 +1,72 @@
 from random import randint
 
-N = 3
-M = 5
-A = []
-B = []
 
-for i in range(N):
-    for x in range(M):
-        B.append(randint(-10, 10))
-    A.append(B)
-    B = []
+def make_matrix(n, m):
+    a = []
+    b = []
+    for i in range(n):
+        for x in range(m):
+            b.append(randint(-10, 10))
+        a.append(b)
+        b = []
 
-for i in range(len(A)):
-    for j in range(len(A[i])):
-        print(A[i][j], end=' ')
-    print()
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            print(f'({a[i][j]})', end='  ')
+        print()
+    return a
 
 
-for z in range(len(A)):
-    max_number = None
-    for y in range(len(A[z])):
-        if max_number is None or A[z][y] > max_number:
-            max_number = A[z][y]
-    print(max_number, f'max number in {z+1} horizontal line \n')
+def max_horizontal(matrix):
+    max_numbers_info = ''
+    for z in range(len(matrix)):
+        max_number = None
+        for y in range(len(matrix[z])):
+            if max_number is None or matrix[z][y] > max_number:
+                max_number = matrix[z][y]
+        max_numbers_info += f'[{max_number}] - max number in [{z + 1}] horizontal line \n'
+    print('')
+    return max_numbers_info
 
-print('\n')
-for u in range(M):
-    max_number = None
-    for y in range(len(A)):
-        if max_number is None or A[y][u] > max_number:
-            max_number = A[y][u]
-    print(max_number, f'max number in {u + 1} vertical line \n')
+
+def max_vertical(matrix):
+    max_numbers_info = ''
+    for u in range(len(matrix)):
+        max_number = None
+        for y in range(len(matrix[u])):
+            if max_number is None or matrix[y][u] > max_number:
+                max_number = matrix[y][u]
+        max_numbers_info += f'[{max_number}] - max number in [{u + 1}] vertical line \n'
+    print('')
+    return max_numbers_info
+
+
+def min_horizontal(matrix):
+    min_numbers_info = ''
+    for u in range(len(matrix)):
+        min_number = None
+        for y in range(len(matrix[u])):
+            if min_number is None or matrix[u][y] < min_number:
+                min_number = matrix[u][y]
+        min_numbers_info += f'[{min_number}] - min number in [{y + 1}] horizontal line \n'
+    print('')
+    return min_numbers_info
+
+
+def min_vertical(matrix):
+    min_numbers_info = ''
+    for u in range(len(matrix)):
+        min_number = None
+        for y in range(len(matrix[u])):
+            if min_number is None or matrix[y][u] < min_number:
+                min_number = matrix[y][u]
+        min_numbers_info += f'[{min_number}] - min number in [{u + 1}] vertical line \n'
+    print('')
+    return min_numbers_info
+
+
+my_matrix = make_matrix(3, 3)
+print(max_horizontal(my_matrix))
+print(max_vertical(my_matrix))
+print(min_horizontal(my_matrix))
+print(min_vertical(my_matrix))
