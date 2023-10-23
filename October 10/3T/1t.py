89158692b649b4d5c1b72953dce3c849cf8546db
yngkzk <131941774+yngkzk@users.noreply.github.com>
2023-10-23 19:25:30 +0600

    re

--- October 10/3T/1t.py
+++ October 10/3T/1t.py
@@ -5,29 +5,27 @@ import sys
 with open(sys.argv[1], encoding='utf-8') as file:
     text = file.read()
 
-def find_value(data):
+def find_and_convert_value(data):
     patterns = [r'([0-9]*?.?[0-9] [A-Z]{3})', r'([0-9]*[.]?[0-9]+?[$€])']
-    value = []
+    result = []
 
     for pattern in patterns:
-        value += (re.findall(pattern, data))
-    return value
-
-value = find_value(text)
-
-def convert_value(data):
-    for value in data:
-        if value.endswith('$') or value.endswith('USD'):
-            number = float(re.sub('[$]| USD', '', value))
-            number *= 476.84
-            data[data.index(value)] = str(round(number, 2)) + '₸' 
-        elif value.endswith('€') or value.endswith('EUR'):
-            number = float(re.sub('[€]| EUR' , '', value))
-            number *= 501.99
-            data[data.index(value)] = str(round(number, 2)) + '₸' 
+        result += (re.findall(pattern, data))
+        for value in result:
+            if value.endswith('$') or value.endswith('USD'):
+                number = float(re.sub('[$]| USD', '', value))
+                number *= 476.84
+                number = str(round(number, 2)) + '₸'
+                # result[result.index(value)] = str(round(number, 2)) + '₸' 
+                data = data.replace(value, number)
+            elif value.endswith('€') or value.endswith('EUR'):
+                number = float(re.sub('[€]| EUR' , '', value))
+                number *= 501.99
+                number = str(round(number, 2)) + '₸'
+                data = data.replace(str(value), str(number))
     return data
 
-converted_value = convert_value(value)
+value = find_and_convert_value(text)
 
 with open(sys.argv[2], 'w', encoding='utf-8') as secondFile:
-    secondFile.write(' '.join(converted_value))
+    secondFile.write(value)
